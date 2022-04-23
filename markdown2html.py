f1926382629140060ed0bc1c10195b84743b6c43
mbukeRepo <mbuke404@gmail.com>
2022-04-23 09:34:31 +0200

    adjustments to pass the second checker

--- markdown2html.py
+++ markdown2html.py
@@ -12,14 +12,20 @@ from write_to_file import write_to_file
 def main():
     """ main enty point to my program """
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         exit(1)
     try:
         markdown = open(sys.argv[1])
+        html = []
+        for line in markdown:
+            html.append(parse_heading(line))
+
         dest_file = open(sys.argv[2], 'w')
+        write_to_file(dest_file, html)
+
         dest_file.close()
         markdown.close()
-        exit(0)
     except FileNotFoundError:
         print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         exit(1)
